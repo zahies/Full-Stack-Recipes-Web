9305759fb65e30d9669ab3b226bde3e4d11cfab3
zahies <kaprizahi@gmail.com>
2020-07-12 18:24:40 +0300

    httpOnly

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-
-var express = require('express');
-var morgan = require("./node_modules/morgan")
-var bodyParser = require("./node_modules/body-parser")
-var session = require("client-sessions")
-var cors = require('./node_modules/cors')
-//routes: 
-const user = require("./routes/user")
-const search = require("./routes/search")
-const recipes = require("./routes/recipes")
-const auth = require("./routes/auth")
-
-
+var express = require("express");
+var morgan = require("./node_modules/morgan");
+var bodyParser = require("./node_modules/body-parser");
+var session = require("client-sessions");
+var cors = require("./node_modules/cors");
+//routes:
+const user = require("./routes/user");
+const search = require("./routes/search");
+const recipes = require("./routes/recipes");
+const auth = require("./routes/auth");
 
 const app = express();
-const port =  process.env.PORT || "4000";
-
+const port = process.env.PORT || "4000";
 
 app.use(
-    session({
-        cookieName: "session",
-        secret: "123bimbam",
-        duration: 20 * 1000
-        
-    })
+  session({
+    cookieName: "session",
+    secret: "123bimbam",
+    duration: 20 * 1000,
+    cookie: {
+      httpOnly: false,
+    },
+  })
 );
 
 const corsConfig = {
-    origin: true,
-    credentials: true
-  };
-  
+  origin: true,
+  credentials: true,
+};
+
 app.use(cors(corsConfig));
 app.options("*", cors(corsConfig));
 //Vue.use(VueAxios, axios);
@@ -41,8 +39,6 @@ app.use("/search", search);
 
 app.use(auth);
 
-
-
 // //parse app
 // app.use(bodyParser.urlencoded({ extended : false}))
 
@@ -52,38 +48,22 @@ app.use(auth);
 // //prints to the log
 // app.use(morgan(":method :url :status  :response-time ms"))
 
-
 // //settting cookies config:
 
-
-
-
-
-
-
-app.get('/alive', (req,res) => {
-    res.status(200).send(" i'm alive !!")
+app.get("/alive", (req, res) => {
+  res.status(200).send(" i'm alive !!");
 });
 
-app.get('/', (req, res) => {
-    req.session.id = "test";
-	res.status(200).send("Hello World");
+app.get("/", (req, res) => {
+  req.session.id = "test";
+  res.status(200).send("Hello World");
 });
 
-
-
-app.get( (req, res) => {
-    res.status(401).send(" page not found :( ");
+app.get((req, res) => {
+  res.status(401).send(" page not found :( ");
 });
-
-
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-    console.log("api key : " + process.env.spooncular_apiKey)
+  console.log(`Listening on port ${port}`);
+  console.log("api key : " + process.env.spooncular_apiKey);
 });
-
-
-
-
-
